da1268afaacde589d9dd7e7a7c54b26829a02543
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-11-15 20:33:24 +0100

    update

---
+++ styles.js/first.modul.js
@@ -0,0 +1,22 @@
+// преобразовали строку в число
+// let quantiny = prompt("Enter number");
+// quantiny = Number(quantiny);
+
+//Number.parseInt() - парсить з рядка ціле число
+// let elementWidth = "50px";
+// elementWidth = Number.parseInt(elementWidth);
+// console.log(elementWidth);
+
+// Number.parseFloat() - парсить з рядка дробове число
+// let elementHeight = "266.988878px";
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log(elementHeight);
+
+// toFixed() = аби результат скоротити до певного знаку після коми. Указываем до скольки знаков после запятой сократить значение. После получаем
+// строку и через Намбер переводим его в число
+// let salary = 126.283872;
+// // salary = salary.toFixed(2);
+// // salary = Number(salary);
+// // вариант покороче
+// salary = Number(salary.toFixed(2));
+// console.log(salary);
---
+++ styles.js/test.js
@@ -0,0 +1,22 @@
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// 2
+// let password = "";
+
+// do {
+//   password = prompt("Введіть пароль довший 4-х символів", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+
+// 3
+// const max = 10;
+// for (let i = 0; i < max; i += 1) {
+//   console.log(`${max} % ${i} = `, max % i);
+// }
