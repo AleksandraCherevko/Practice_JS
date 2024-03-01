5adec1b5b9d9d62d84554032932feedf6b682f37
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-03-01 12:45:44 +0100

    upd

--- styles.js/modul-six.js
+++ styles.js/modul-six.js
@@ -174,12 +174,171 @@
 // }
 
 // showBooks(library.logBookCount.bind(library));
-const a = () => {
-  console.log(this);
-};
+// const a = () => {
+//   console.log(this);
+// };
 
-function b() {
-  a();
+// function b() {
+//   a();
+// }
+
+// b.call({ user: "Mango" });
+
+// const animal = {
+//   legs: 4,
+// };
+
+// const dog = Object.create(animal);
+// dog.name = "Mango";
+
+// console.log(dog); // { name: "Mango", [[Prototype]]: animal }
+
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(child);
+
+// const customer = {
+//   username: "Jacob",
+// };
+
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(customer);
+// dog.name = "Mango";
+// console.log(dog);
+
+// console.log(animal.hasOwnProperty(dog));
+// console.log(dog.hasOwnProperty(customer));
+
+// const animal = { legs: 4 };
+// const dog = Object.create(animal);
+// dog.name = "Mango";
+
+// for (const key in dog) {
+//   if (dog.hasOwnProperty(key)) {
+//     console.log(key); // "name"
+//   }
+// }
+
+// const objC = { c: "objC prop" };
+
+// const objB = Object.create(objC);
+// objB.b = "objB prop";
+
+// const objA = Object.create(objB);
+// objA.a = "objA prop";
+
+// console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+// console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+// console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+
+// const apartment = {
+//   rooms: 4,
+//   floor: 2,
+//   taxFree: false,
+// };
+
+// const condo = Object.create(apartment);
+// condo.rooms = 3;
+
+// console.log(condo.rooms);
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(ancestor.isPrototypeOf(parent));
+// console.log(parent.isPrototypeOf(child));
+
+// class Car {}
+// const newCar = new Car();
+// console.log(newCar);
+
+// class Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+// const newCar = new Car();
+// console.log(new Car("BMW", "X5", 58900));
+
+// class Car {
+//   constructor(value) {
+//     this.brand = value.brand;
+//     this.model = value.model;
+//     this.price = value.price;
+//   }
+// }
+// const newCar = new Car({ brand: "Nissan", model: "Murano", price: 31700 });
+// console.log(newCar);
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   // Метод getEmail
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   // Метод changeEmail
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Mango",
+//   email: "mango@gmail.com",
+// });
+
+// console.log(mango.getEmail());
+
+class Car {
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+
+  changePrice(newPrice) {
+    this.price = newPrice;
+  }
 }
 
-b.call({ user: "Mango" });
+const updateValue = new Value({
+  price: "100",
+});
+
+console.log(updateValue.getPrice());
